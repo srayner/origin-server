dfc4fc5e64778efa06994397e36aa576a38f0883
Steve Rayner <srayner02@gmail.com>
2019-02-02 09:29:04 +0000

    Trees now use strings for the _id field.

--- app/routes/tree-routes.js
+++ app/routes/tree-routes.js
@@ -1,7 +1,9 @@
 var ObjectID = require("mongodb").ObjectID;
 
 module.exports = function(app, db) {
-  // INDEX Trees
+  /**
+   * Returns all trees.
+   */
   app.get("/trees", (req, res) => {
     db.collection("trees")
       .find({})
@@ -14,7 +16,9 @@ module.exports = function(app, db) {
       });
   });
 
-  // CREATE Tree
+  /**
+   * Creates a new tree.
+   */
   app.post("/trees", (req, res) => {
     const tree = req.body;
     db.collection("trees").insertOne(tree, (err, result) => {
@@ -26,10 +30,12 @@ module.exports = function(app, db) {
     });
   });
 
-  // READ Tree
+  /**
+   * Returns a single tree.
+   */
   app.get("/trees/:id", (req, res) => {
     const id = req.params.id;
-    const details = { _id: new ObjectID(id) };
+    const details = { _id: id };
     db.collection("trees").findOne(details, (err, item) => {
       if (err) {
         res.send({ error: "An error has occured." });
@@ -39,10 +45,12 @@ module.exports = function(app, db) {
     });
   });
 
-  // UPDATE Tree
+  /**
+   * Updates a tree.
+   */
   app.patch("/trees/:id", (req, res) => {
     const id = req.params.id;
-    const query = { _id: new ObjectID(id) };
+    const query = { _id: id };
     const update = {
       $set: {
         name: req.body.name
@@ -57,10 +65,12 @@ module.exports = function(app, db) {
     });
   });
 
-  // DELETE Tree
+  /**
+   * Deletes a tree.
+   */
   app.delete("/trees/:id", (req, res) => {
     const id = req.params.id;
-    const details = { _id: new ObjectID(id) };
+    const details = { _id: id };
     db.collection("trees").remove(details, (err, item) => {
       if (err) {
         res.send({ error: "An error occured" });
