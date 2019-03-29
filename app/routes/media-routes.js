23c6688a60afca4788534983de1458afc25bcb45
Steve Rayner <srayner02@gmail.com>
2019-03-29 22:51:28 +0000

    uploaded file now gets moved and an entry is stored in the database

--- app/routes/media-routes.js
+++ app/routes/media-routes.js
@@ -1,16 +1,25 @@
 const IncomingForm = require("formidable").IncomingForm;
+const fs = require("fs");
 
 module.exports = function(app, db) {
   // uplad media
   app.post("/media", (req, res) => {
     const form = new IncomingForm();
     form.on("file", (field, file) => {
-      // Do something with the file
-      // e.g. save it to the database
-      // you can access it using file.path
+      const path = __dirname + "/../media/uploaded_file";
+      fs.rename(file.path, path, function(err) {
+        if (err) throw err;
+        const media = {
+          originalName: file.name,
+          type: file.type,
+          size: file.size,
+          path: path
+        };
+        db.collection("media").insertOne(media, (err, result) => {});
+      });
     });
     form.on("end", () => {
-      res.json();
+      res.json({ message: "ok" });
     });
     form.parse(req);
   });
